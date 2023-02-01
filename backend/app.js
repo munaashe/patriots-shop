48f6f9e0fa64445cbb2a0010989d02e91a1cd4e2
Denis Siduna <mnashesduna@gmail.com>
2023-02-01 22:33:13 +0200

    handling dev and prod errors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const errorMiddleware = require('./middlewares/errors')
+
 app.use(express.json())
 
 //importing all routes
@@ -8,5 +10,9 @@ const products = require('./routes/product')
 
 
 app.use('/api/v1', products)
+
+
+//error handling middleware
+app.use(errorMiddleware);
 
 module.exports = app
